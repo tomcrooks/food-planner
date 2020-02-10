ea9e06713f5f809cd571f71808d574e3e2c1d280
Tom Crooks <thomas.crooks@pimberly.com>
2020-02-10 17:28:29 +0000

    meh

--- server/server.js
+++ server/server.js
@@ -6,17 +6,24 @@ require('dotenv').config()
 
 const app = express()
 const port = process.env.PORT || 5000
-const DB_URL = process.env.DB_URL
 
 app.use(cors())
 app.use(express.json())
 
-app.listen(port, () => {
+app.listen(port, async () => {
     console.log(`Server is running on port ${port}...`)
 
-    // Connect to the db
-    MongoClient.connect(DB_URL, { useUnifiedTopology: true }, (err, db) => {
-        if (!err) console.log("DB Connection established");
-    });
+    const client = new MongoClient(process.env.DB_URL, { useNewUrlParser: true , useUnifiedTopology: true });
+
+    try {
+        await client.connect();
+        console.log(`Establed connection to the database.`)
+
+        // const db = client.db(process.env.DB_NAME);
+    } catch (err) {
+        console.log(err.stack);
+    }
+
+    client.close();
 
 })
